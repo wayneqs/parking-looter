adb739993e34ec9ccdff2ce9e0236e9033f36350
Wayne Quinlivan <wayneqs@gmail.com>
2016-10-25 00:43:28 +0100

    virgin

---
+++ looter.py
@@ -0,0 +1,44 @@
+from jinja2 import Environment, FileSystemLoader
+from datetime import datetime, timedelta
+import os, smtplib, calendar
+from email.mime.text import MIMEText
+
+def loadTemplate():
+    env = Environment(loader=FileSystemLoader('templates'))
+    template = env.get_template('email.txt')
+    return template
+
+def dateString(date):
+    return date.strftime("%d %b %Y")
+
+def calculateNextWeekday(date, weekday):
+    days_until_next_weekday = 7 - date.weekday() + weekday
+    next_weekday = (today + timedelta(days=days_until_next_weekday)).date()
+    return dateString(next_weekday)
+
+def getLooterName():
+    return os.environ['LOOTER_NAME']
+
+def getLooterEmail():
+    return os.environ['LOOTER_EMAIL']
+
+def getLootControllerEmail():
+    return os.environ['LOOT_CONTROLLER_EMAIL']
+
+def sendEmail(text):
+    msg = MIMEText(text)
+    msg['Subject'] = 'Motorcycle parking booking'
+    msg['From'] = getLooterEmail()
+    msg['To'] = getLootControllerEmail()
+    s = smtplib.SMTP('localhost')
+    s.sendmail(getLooterEmail(), getLootControllerEmail(), msg.as_string())
+    s.quit()
+
+calendar.setfirstweekday(calendar.MONDAY)
+today = datetime.now()
+variables = {}
+variables['looter_name'] = getLooterName()
+variables['start_date'] = calculateNextWeekday(today, calendar.MONDAY)
+variables['end_date'] = calculateNextWeekday(today, calendar.FRIDAY)
+
+sendEmail(loadTemplate().render(variables))
